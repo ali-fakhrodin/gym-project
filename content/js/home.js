8bf6e53c9bc7bfaf6a079e3bcc76224ac1be058c
AliFakhrodin <alifakhrodin16@gmail.com>
2024-07-27 11:29:21 +0330

    add & config type-writer

--- content/js/home.js
+++ content/js/home.js
@@ -10,6 +10,27 @@ const toggleXIcon = $.querySelector('.toggle-x-i')
 
 const scrollHightLine = window.getComputedStyle($.body, '::before')
 
+// ---------------------------- Type-writer ----------------------------
+
+let mainTwriterTxt = document.querySelector('.section-top h1');
+
+let typewriter = new Typewriter(mainTwriterTxt, {
+     loop: true,
+     delay: 25,
+     deleteSpeed: 15
+});
+
+typewriter
+     .pauseFor(500)
+     .typeString(`دریافت آنلاین <mark style="font-size: 2.6rem;">برنامه غذایی</mark>
+          &nbsp; و &nbsp; <mark style="font-size: 2.6rem;">بدنسازی</mark> + پشتیبانی رایگان`)
+     .pauseFor(2000)
+     .deleteChars(15)
+     .changeDelay(260)
+     .typeString(`VIP`)
+     .pauseFor(2000)
+     .start();
+
 // ------------------------------- Events -------------------------------
 
 dietBox.addEventListener('click', () => {
